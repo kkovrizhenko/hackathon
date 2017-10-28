0243837e8535d0b62ca75052ce1242053c34cd08
beesteek <32173603+beesteek@users.noreply.github.com>
2017-10-28 05:05:21 +0300

    menu add

---
+++ js/js.js
@@ -0,0 +1,6 @@
+$( ".slide-menu-togle" ).click(function() {
+    $(".menu").toggleClass('menu-isclosed');
+    $(".slide-menu").toggleClass('slide-menu-isclosed');
+    $(".slide-menu-contacts").toggleClass('slide-menu-contacts-isclosed');
+    $(".slide-menu-icon").toggleClass('slide-menu-icon-isclosed');
+});
